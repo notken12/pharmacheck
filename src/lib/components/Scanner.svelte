7e27e45d07616f5ce6d1fc839894741b7c1ffef3
Ken Zhou <kendotzhou@gmail.com>
2023-12-10 01:17:39 +0000

    use dialog

--- src/lib/components/Scanner.svelte
+++ src/lib/components/Scanner.svelte
@@ -3,6 +3,7 @@
 
 	import Camera from './Camera.svelte';
 	import { Button } from '$lib/components/ui/button';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
 
 	let scanImage = () => {
 		camera.takePicture();
@@ -10,6 +11,7 @@
 	};
 
 	let camera: Camera;
+	let medicineInfo: { name: string; ingredients: string[] };
 
 	let recognizeText = async () => {
 		const src = camera.getImageData();
@@ -25,13 +27,39 @@
 		const serverResponse = await fetch(
 			'/medicine-info?' + new URLSearchParams({ text: result.data.text })
 		);
-		const medicineInfo = await serverResponse.json();
-		console.log(medicineInfo);
+		medicineInfo = await serverResponse.json();
+		medicineInfoDialogOpen = true;
 	};
 
 	let resultText = '';
+
+	let alertTrigger: Button;
+	let trigger: string;
+
+	let medicineInfoDialogOpen = false;
 </script>
 
 <Camera bind:this={camera}></Camera>
 <Button on:click={scanImage}>Scan</Button>
 <p>{resultText}</p>
+<p></p>
+
+<AlertDialog.Root open={medicineInfoDialogOpen}>
+	<AlertDialog.Content>
+		<AlertDialog.Header>
+			<AlertDialog.Title>Medicine Info</AlertDialog.Title>
+			<AlertDialog.Description>
+				<p>
+					Name: {medicineInfo?.name || 'None'}
+				</p>
+				<p>
+					Active ingredients: {medicineInfo?.ingredients || 'None'}
+				</p>
+			</AlertDialog.Description>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+			<AlertDialog.Action>Continue</AlertDialog.Action>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>
